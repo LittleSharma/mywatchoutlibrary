f18c4312c821e066de420c946a2443c90888894e
LittleSharma <little.sharma@aryavratinfotech.com>
2023-04-07 12:25:45 +0530

    take json String

--- app/src/main/java/com/example/testingcalldetectionlibrary/MainActivity.kt
+++ app/src/main/java/com/example/testingcalldetectionlibrary/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     lateinit var nextButton: Button
-    lateinit var userDetails: UserDetails
+//    lateinit var userDetails: UserDetails
 
     @SuppressLint("LongLogTag")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,19 +29,40 @@ class MainActivity : AppCompatActivity() {
 
         nextButton = findViewById(R.id.next_btn)
 
-        userDetails = UserDetails(UserUser("Shweta@mailinator.com","Shweta", "kumari", "85856895885","Female"),
-            "ola", "1221117105526941", "EmergencyMessage", "p", UserVehicle("1FADP3J27EL153248",
-                "Ford", "RJ16CH7898", "2014","Focus","Ambulance"),
-            "26.9124", "Audio", "75.7873")
+//        userDetails = UserDetails(UserUser("Shweta@mailinator.com","Shweta", "kumari", "85856895885","Female"),
+//            "ola", "1221117105526941", "EmergencyMessage", "p", UserVehicle("1FADP3J27EL153248",
+//                "Ford", "RJ16CH7898", "2014","Focus","Ambulance"),
+//            "26.9124", "Audio", "75.7873")
 
-        val gson = Gson()
-        val json = gson.toJson(userDetails)
-        println(json)
+        val userDetails : String =  "{\n" +
+                "  \"apiAuthrizationKey\": \"1221117105526941\",\n" +
+                "  \"longitude\": 26.9124,\n" +
+                "  \"latitude\": 75.7873,\n" +
+                "  \"tripBy\": \"ola\",\n" +
+                "  \"userType\": \"p\",\n" +
+                "  \"caseIncomingType\":\"Audio\",\n" +
+                "  \"user\": {\n" +
+                "    \"firstName\": \"vicky\",\n" +
+                "    \"lastName\": \"kumar\",\n" +
+                "    \"email\": \"vicky3i@gmail.com\",\n" +
+                "    \"phone\": \"85856895885\",\n" +
+                "    \"gender\": \"Male\"\n" +
+                "    \n" +
+                "  },\n" +
+                "  \"vehicle\": {\n" +
+                "    \"vehYear\": \"2014\",\n" +
+                "    \"vehMake\": \"Ford\",\n" +
+                "    \"vinNum\": \"1FADP3J27EL153248\",\n" +
+                "    \"vehicle_model\": \"Focus\",\n" +
+                "    \"vehicle_number\": \"RJ16CH7898\",\n" +
+                "    \"vehicleType\": \"Ambulance\"\n" +
+                "  }\n" +
+                "}"
         nextButton.setOnClickListener {
             var intent: Intent = Intent()
             intent = Intent(this@MainActivity, Watchout::class.java)
-            intent.putExtra("user_details", json)
-            Log.d("Your_Data", json)
+            intent.putExtra("user_details", userDetails)
+            Log.d("Your_Data", userDetails)
             startActivity(intent)
         }
 
--- mywatchoutlibrary/src/main/java/com/example/mywatchoutlibrary/UserDetails.kt
+++ mywatchoutlibrary/src/main/java/com/example/mywatchoutlibrary/UserDetails.kt
@@ -4,69 +4,69 @@ import com.google.gson.annotations.SerializedName
 
 data class UserDetails(
 
-	@field:SerializedName("user_user")
-	val userUser: UserUser? = null,
-
-	@field:SerializedName("user_tripBy")
-	val userTripBy: String? = null,
-
-	@field:SerializedName("user_authkey")
+	@field:SerializedName("apiAuthrizationKey")
 	val userAuthkey: String? = null,
 
-	@field:SerializedName("user_caseType")
-	val userCaseType: String? = null,
-
-	@field:SerializedName("user_userType")
-	val userUserType: String? = null,
-
-	@field:SerializedName("user_vehicle")
-	val userVehicle: UserVehicle? = null,
-
-	@field:SerializedName("user_longitude")
+	@field:SerializedName("longitude")
 	val userLongitude: Any? = null,
 
-	@field:SerializedName("user_caseIncomingType")
-	val userCaseIncomingType: String? = null,
+	@field:SerializedName("latitude")
+	val userLatitude: Any? = null,
 
-	@field:SerializedName("user_latitude")
-	val userLatitude: Any? = null
+	@field:SerializedName("tripBy")
+	val userTripBy: String? = null,
+
+	@field:SerializedName("userType")
+	val userUserType: String? = null,
+
+	@field:SerializedName("user")
+	val userUser: UserUser? = null,
+
+	@field:SerializedName("caseType")
+	val userCaseType: String? = null,
+
+	@field:SerializedName("vehicle")
+	val userVehicle: UserVehicle? = null,
+
+	@field:SerializedName("caseIncomingType")
+	val userCaseIncomingType: String? = null,
 )
 
 data class UserUser(
 
-	@field:SerializedName("user_email")
-	val userEmail: String? = null,
-
-	@field:SerializedName("user_firstName")
+	@field:SerializedName("firstName")
 	val userFirstName: String? = null,
 
-	@field:SerializedName("user_lastName")
+	@field:SerializedName("lastName")
 	val userLastName: String? = null,
 
-	@field:SerializedName("user_phone")
+	@field:SerializedName("email")
+	val userEmail: String? = null,
+
+	@field:SerializedName("phone")
 	val userPhone: String? = null,
 
-	@field:SerializedName("user_gender")
+	@field:SerializedName("gender")
 	val userGender: String? = null
 )
 
 data class UserVehicle(
 
-	@field:SerializedName("user_vinNum")
-	val userVinNum: String? = null,
-
-	@field:SerializedName("user_vehMake")
-	val userVehMake: String? = null,
-
-	@field:SerializedName("user_vehicle_number")
-	val userVehicleNumber: String? = null,
-
-	@field:SerializedName("user_vehYear")
+	@field:SerializedName("vehYear")
 	val userVehYear: String? = null,
 
-	@field:SerializedName("user_vehicle_model")
+	@field:SerializedName("vehMake")
+	val userVehMake: String? = null,
+
+	@field:SerializedName("vinNum")
+	val userVinNum: String? = null,
+
+	@field:SerializedName("vehicle_number")
+	val userVehicleNumber: String? = null,
+
+	@field:SerializedName("vehicle_model")
 	val userVehicleModel: String? = null,
 
-	@field:SerializedName("user_vehicleType")
+	@field:SerializedName("vehicleType")
 	val userVehicleType: String? = null
 )
